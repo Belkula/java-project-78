537610ef8aef92967d15b7af54460c6673286b73
Belkula <majin.td@mail.ru>
2024-04-11 22:32:22 +0300

    изменение схемы stringschema 2

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -5,18 +5,18 @@ public class StringSchema extends BaseSchema<String> {
 
     @Override
     public StringSchema required() {
-        Predicate<String> req = input ->  input instanceof String &&  input.length() > 0;
+        Predicate<String> req = input ->  input instanceof String &&  ((String) input).length() > 0;
         predicateList.add(req);
         return this;
     }
     public StringSchema minLength(int length) {
-        Predicate<String> minlen = (input) -> input.length() >= length;
+        Predicate<String> minlen = (input) -> ((String) input).length() >= length;
         predicateList.add(minlen);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        Predicate<String> substr = (input) ->  input.contains(substring);
+        Predicate<String> substr = (input) ->  ((String) input).contains(substring);
         predicateList.add(substr);
         return this;
     }
